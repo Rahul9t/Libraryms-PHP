574e9f4c8fe0cc4f3a80d02f5f3bf9c42c11841c
Rahul9t <69580540+Rahul9t@users.noreply.github.com>
2023-12-20 18:34:54 +0530

    Update index.php

--- index.php
+++ index.php
@@ -19,14 +19,14 @@
 		<div id="allTheThings">
 			<div id="member">
 				<a href="member">
-					<img src="img/ic_membership.svg" width="250px" height="auto"/><br />
+					<img src="img/id-card-svgrepo-com.svg" width="250px" height="auto"/><br />
 					&nbsp;Member Login
 				</a>
 			</div>
 			<div id="verticalLine">
 				<div id="librarian">
 					<a id="librarian-link" href="librarian">
-						<img src="img/ic_librarian2.svg" width="250px" height="220" /><br />
+						<img src="img/library-svgrepo-com.svg" width="250px" height="220" /><br />
 						&nbsp;&nbsp;&nbsp;Librarian Login
 					</a>
 				</div>
